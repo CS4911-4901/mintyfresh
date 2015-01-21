ae5dda2d843b783b69cf75800db385efe3858161
OzuYatamutsu <scollins36@gatech.edu>
2015-01-21 00:17:46 -0500

    Start database config

---
+++ app/src/androidTest/java/edu/gatech/cs4911/mintyfresh/db/DatabaseConfig.java
@@ -0,0 +1,54 @@
+package edu.gatech.cs4911.mintyfresh.db;
+
+/**
+ * A grouped set of configurations to connect to an external database.
+ */
+public class DatabaseConfig {
+    /**
+     * The username to authenticate against the database.
+     */
+    protected String username;
+    /**
+     * The password to authenticate against the database.
+     */
+    protected String password;
+    /**
+     * The hostname to connect to.
+     */
+    protected String hostname;
+    /**
+     * The port number to open a remote socket to.
+     */
+    protected int port;
+    /**
+     * The database to connect to on the remote host.
+     */
+    protected String database;
+
+    /**
+     * Initializes a new DatabaseConfig with given attributes.
+     *
+     * @param username The username to authenticate with.
+     * @param password The password to authenticate with.
+     * @param hostname The hostname of the remote database to connect to.
+     * @param port The port number to establish a remote socket on.
+     * @param database The database to connect to.
+     */
+    public DatabaseConfig(String username, String password, String hostname,
+                          int port, String database) {
+
+        this.username = username;
+        this.password = password;
+        this.hostname = hostname;
+        this.port = port;
+        this.database = database;
+    }
+}
+
+/**
+ * Static configuration to connect to MySQL Databases.
+ */
+class StaticConfig {
+    public static DatabaseConfig STEAKSCORP_READ_ONLY = new DatabaseConfig("cs49114901-read",
+            "1c792_e03de141", "steakscorp.org", 3306, "4901mintyfresh");
+}
