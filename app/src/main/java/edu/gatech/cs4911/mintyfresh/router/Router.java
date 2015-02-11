8ea70b52f4fab7169982b92ac9296d4851d95ab6
OzuYatamutsu <scollins36@gatech.edu>
2015-02-10 20:04:44 -0500

    getCurentLocation()

--- app/src/main/java/edu/gatech/cs4911/mintyfresh/router/Router.java
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/router/Router.java
@@ -1,8 +1,19 @@
 package edu.gatech.cs4911.mintyfresh.router;
 
+import android.location.Location;
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * A Router is a wrapper around calls to the Google Maps API.
  */
 public class Router {
-    // TODO: implement
+
+    /**
+     * Returns the current location as a LatLng object.
+     *
+     * @return The current location as a LatLng object.
+     */
+    public static LatLng getCurrentLocation(Location location) {
+        return new LatLng(location.getLatitude(), location.getLongitude());
+    }
 }
