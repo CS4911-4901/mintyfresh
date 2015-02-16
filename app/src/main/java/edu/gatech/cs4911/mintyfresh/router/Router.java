119d18003707cbbb60dbcd4931649380d7cbfd8c
OzuYatamutsu <scollins36@gatech.edu>
2015-02-16 10:40:13 -0500

    Attempt at JSON parsing, but we need to test!

--- app/src/main/java/edu/gatech/cs4911/mintyfresh/router/Router.java
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/router/Router.java
@@ -5,6 +5,9 @@ import static edu.gatech.cs4911.mintyfresh.router.WebQuerier.getHttpDoc;
 import android.location.Location;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 
 import java.io.IOException;
@@ -44,9 +47,17 @@ public class Router {
     public static List<LatLng> getDirectionsTo(float startLatitude, float startLongitude,
         float destLatitude, float destLongitude) throws IOException {
 
+        List<LatLng> result;
         String uri = constructMapUri(startLatitude, startLongitude, destLatitude, destLongitude);
 
-        return parseJsonToLatLng(getHttpDoc(uri));
+        try {
+            result = parseJsonToLatLng(getHttpDoc(uri));
+        } catch (JSONException e) {
+            // No points in result, so we assume no routable path
+            return null;
+        }
+
+        return result;
     }
 
     /**
@@ -71,12 +82,65 @@ public class Router {
      *
      * @param doc The Document object returned from the server.
      * @return A List of LatLng objects constructed from the response.
+     * @throws JSONException if there was an issue parsing the response.
      */
-    private static List<LatLng> parseJsonToLatLng(Document doc) {
+    private static List<LatLng> parseJsonToLatLng(Document doc) throws JSONException {
         List<LatLng> output = new ArrayList<>();
-        // TODO: Complete
+        JSONObject responseBody = new JSONObject(doc.toString());
+
+        // First route of response
+        JSONObject route = responseBody.getJSONArray("routes").getJSONObject(0);
+
+        // All legs of route
+        JSONArray legs = route.getJSONArray("legs");
+        for (int j = 0; j < legs.length(); j++) {
+            // For each leg, add all points to list
+
+            // Starting from the start and end points...
+            LatLng[] startEnd = constructLatLngFromJson(legs.getJSONObject(j));
+
+            // Absolute starting location
+            output.add(startEnd[0]);
+
+            // And all points in between:
+            JSONArray steps = legs.getJSONObject(j).getJSONArray("steps");
+
+            for (int i = 0; i < steps.length(); i++) {
+                LatLng[] stepStartEnd = constructLatLngFromJson(steps.getJSONObject(i));
+
+                // Start point
+                output.add(stepStartEnd[0]);
+                // End point
+                output.add(stepStartEnd[1]);
+            }
+
+            // Absolute ending location
+            output.add(startEnd[1]);
+        }
 
         return output;
+    }
+
+    /**
+     * Constructs a new LatLng array from a Google Maps JSON element containing
+     * a start and end location. The first element of the returned array will
+     * be the start location, and the last element of the returned array will
+     * be the end location.
+     *
+     * @param latlng The JSONObject containing "start_location" and "end_location" fields.
+     * @return A new LatLng array with start and end points, respectively.
+     * @throws JSONException if the input object does not contain the required fields.
+     */
+    private static LatLng[] constructLatLngFromJson(JSONObject latlng) throws JSONException {
+        LatLng[] result = new LatLng[2];
+        result[0] = new LatLng(
+                latlng.getJSONObject("start_location").getLong("lat"),
+                latlng.getJSONObject("start_location").getLong("lng"));
+        result[1] = new LatLng(
+                latlng.getJSONObject("start_location").getLong("lat"),
+                latlng.getJSONObject("start_location").getLong("lng"));
+
+        return result;
     }
 
 }
