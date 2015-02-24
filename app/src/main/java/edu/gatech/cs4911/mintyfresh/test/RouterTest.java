c18be243630ec074cc49339cbe415a38f3ec4bda
OzuYatamutsu <scollins36@gatech.edu>
2015-02-23 20:51:52 -0500

    Add Router JUnit test

---
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/test/RouterTest.java
@@ -0,0 +1,28 @@
+package edu.gatech.cs4911.mintyfresh.test;
+
+import android.test.InstrumentationTestCase;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+import static edu.gatech.cs4911.mintyfresh.router.Router.getDirectionsTo;
+
+public class RouterTest extends InstrumentationTestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception { }
+
+    public void testGetCurrentLocation() throws Exception {
+
+    }
+
+    public void testGetDirectionsTo() throws Exception {
+        // Student Center -> CULC
+        List<LatLng> output = getDirectionsTo(33.774063f, -84.398836f, 33.774792f, -84.396386f);
+        assertNotNull(output);
+    }
+}
