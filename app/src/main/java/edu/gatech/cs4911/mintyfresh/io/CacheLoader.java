ed12f1bbfb272b8ec423c050211b3b95514cdf25
OzuYatamutsu <scollins36@gatech.edu>
2015-03-10 16:15:46 -0400

    Implemented methods

--- app/src/main/java/edu/gatech/cs4911/mintyfresh/io/CacheLoader.java
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/io/CacheLoader.java
@@ -1,6 +1,13 @@
 package edu.gatech.cs4911.mintyfresh.io;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A CacheLoader handles the loading of images and hashes
@@ -17,15 +24,72 @@ public class CacheLoader {
      */
     public static final String LOCAL_HASHFILE_PATH = "";
 
-    // TODO: implement
-    public static List<String> loadImages(String path) { return null; }
+    /**
+     * Returns a list of image file names in the given path.
+     *
+     * @param path A given path to look for images.
+     * @return A list of image file names in the given path.
+     */
+    public static List<String> loadImages(String path) {
+        List<String> output = new ArrayList<>();
+        File[] folder = new File(path).listFiles();
+        for (File image : folder) {
+            if (image.isFile()) {
+                output.add(image.getName());
+            }
+        }
+
+        return output;
+    }
+
+    /**
+     * Returns a list of image file names in the default path.
+     *
+     * @return A list of image file names in CacheLoader.LOCAL_IMAGE_PATH.
+     */
     public static List<String> loadImages() {
         return loadImages(LOCAL_IMAGE_PATH);
     }
 
-    // TODO: implement
-    public static List<String> loadHashes(String hashFile) { return null; }
-    public static List<String> loadHashes() {
+    /**
+     * Returns a map of image file names to hashes, loaded
+     * from a local hash file. Returns null if the hash file
+     * does not exist or is empty.
+     *
+     * @param hashFile The path to a local hash file.
+     * @return A map of image file names to hashes.
+     */
+    public static Map<String, String> loadHashes(String hashFile) {
+        /**
+         * Format of file: filename:hash
+         */
+        Map<String, String> fileHashes = new HashMap<>();
+        try {
+            // Read file line-by-line and populate map
+            BufferedReader reader = new BufferedReader(new FileReader(hashFile));
+            for (String line; (line = reader.readLine()) != null;) {
+                // "image.svg:hash123" -> <K="image.svg": V="hash123">
+                fileHashes.put(line.split(":")[0], line.split(":")[1]);
+            }
+
+            // We're done with the file!
+            reader.close();
+        } catch (IOException e) {
+            return null;
+        }
+
+        // Return null if there was nothing useful in the file!
+        return (fileHashes.size() > 0) ? fileHashes : null;
+    }
+
+    /**
+     * Returns a map of image file names to hashes, loaded
+     * from the default hash file path.Returns null if the
+     * hash file does not exist or is empty.
+     *
+     * @return The map contained in CacheLoader.LOCAL_HASHFILE_PATH.
+     */
+    public static Map<String, String> loadHashes() {
         return loadHashes(LOCAL_HASHFILE_PATH);
     }
 }
