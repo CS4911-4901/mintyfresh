a48353d2579cda276c5d6887ae8888a28250a83b
kharlan3 <kharlan3@gatech.edu>
2015-03-11 00:37:09 -0400

    Attempt to start hooking up buttons.  not sure why my emulator won't work.

--- app/src/main/java/edu/gatech/cs4911/mintyfresh/HomeScreenActivity.java
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/HomeScreenActivity.java
@@ -1,6 +1,8 @@
 package edu.gatech.cs4911.mintyfresh;
 
+import android.app.Activity;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -26,6 +28,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 import edu.gatech.cs4911.mintyfresh.db.DBHandler;
+import edu.gatech.cs4911.mintyfresh.exception.NoDbResultException;
 import edu.gatech.cs4911.mintyfresh.router.RelativeBuilding;
 
 import static edu.gatech.cs4911.mintyfresh.db.DatabaseConfig.STEAKSCORP_READ_ONLY;
@@ -37,25 +40,13 @@ public class HomeScreenActivity extends ActionBarActivity {
 //    http://theopentutorials.com/tutorials/android/listview/android-expandable-list-view-example/
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private ExpandableListView expListView;
-    private AmenityFinder amenityFinder;
+    protected AmenityFinder amenityFinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        setUpMapIfNeeded();
-        DBHandler dbh;
 
-        try {
-            dbh = new DBHandler(STEAKSCORP_READ_ONLY);
-            amenityFinder = new AmenityFinder(dbh);
-
-        }
-        catch (Exception e) {
-            //todo oh sad day
-            Log.e("boom", "54");
-            amenityFinder = null;
-        }
         expListView = (ExpandableListView) findViewById(R.id.buildingList);
         expListView.setVisibility(View.GONE);
 
@@ -106,109 +97,97 @@ public class HomeScreenActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    protected Activity returnThis() {
+        return this;
+    }
+
     private void expandMenu(int type) {
         //do nothing
         //find the amenities
         //make them into the list view
         //post them
 
-        //showing button-name
-        String name = "";
-        if (type == 0) {
-            name = "Bathrooms";
-        }
-        else if (type == 1) {
-            name = "Vending";
-        }
-        else {
-            name = "Printers";
-        }
-
         Log.v("sigh", "yeah");
 
         Location curLocation = new Location("TEST_PROVIDER");
         curLocation.setLatitude(33.7751878);
         curLocation.setLongitude(-84.39687341);
-        Log.v("hsa", "yes");
-        Log.v("hsa", amenityFinder.toString());
-        Log.v("hsa", "no");
 
-        try {
+        new ConnectToDB(type).execute(curLocation);
+    }
 
-            PriorityQueue<RelativeBuilding> buildingsPQ = amenityFinder.getNearbyBuildings(curLocation);
-            ArrayList<RelativeBuilding> buildings = new ArrayList<RelativeBuilding>();
-            Map<RelativeBuilding, List<Integer>> map = new ArrayMap<RelativeBuilding, List<Integer>>();
+    protected void showEFLA(ArrayList<RelativeBuilding> buildings, Map<RelativeBuilding, List<Integer>> map) {
+        final ExpandableFloorListAdapter expListAdapter = new ExpandableFloorListAdapter(
+                this, buildings, map);
+        expListView.setAdapter(expListAdapter);
 
-            List<Integer> floors;
-            while (!buildingsPQ.isEmpty()) {
-                RelativeBuilding rb = buildingsPQ.poll();
-                buildings.add(rb);
-                floors = amenityFinder.getFloorsInBuilding(rb.getBuilding());
-                map.put(rb, floors);
+        expListView.setVisibility(View.VISIBLE);
+        Log.v("SHOWEFLA", "DONE?");
+    }
+
+
+    private class ConnectToDB extends AsyncTask <Location, Integer, Void> {
+
+        private String name;
+        private int type;
+        private ArrayList<RelativeBuilding> buildings;
+        private Map<RelativeBuilding, List<Integer>> map;
+
+        public ConnectToDB(int type) {
+            this.type = type;
+            //showing button-name
+            String name = "";
+            if (type == 0) {
+                name = "Bathrooms";
             }
-
-            LinearLayout showing = (LinearLayout) findViewById(R.id.showingLayout);
-            Button showingButton = (Button) findViewById(R.id.selectedButton);
-            showingButton.setText("All " + name); //it'll need a little arrow buddy
-            showing.setVisibility(View.VISIBLE);
-
-            Log.v("button", type + "clicked");
-
-            final ExpandableFloorListAdapter expListAdapter = new ExpandableFloorListAdapter(
-                    this, buildings, map);
-            expListView.setAdapter(expListAdapter);
-
-            expListView.setVisibility(View.VISIBLE);
+            else if (type == 1) {
+                name = "Vending";
+            }
+            else {
+                name = "Printers";
+            }
         }
-        catch (Exception e) {
-            Log.v("hsa", e.getMessage());
-            //todo sigh
+
+        @Override
+        protected Void doInBackground(Location... params) {
+            DBHandler dbh;
+
+            try {
+
+                dbh = new DBHandler(STEAKSCORP_READ_ONLY);
+                amenityFinder = new AmenityFinder(dbh);
+
+                PriorityQueue<RelativeBuilding> buildingsPQ = amenityFinder.getNearbyBuildings(params[0]);
+                buildings = new ArrayList<RelativeBuilding>();
+                map = new ArrayMap<RelativeBuilding, List<Integer>>();
+
+                List<Integer> floors;
+                while (!buildingsPQ.isEmpty()) {
+                    RelativeBuilding rb = buildingsPQ.poll();
+                    buildings.add(rb);
+                    floors = amenityFinder.getFloorsInBuilding(rb.getBuilding());
+                    map.put(rb, floors);
+                }
+
+                LinearLayout showing = (LinearLayout) findViewById(R.id.showingLayout);
+                Button showingButton = (Button) findViewById(R.id.selectedButton);
+                showingButton.setText("All " + name); //it'll need a little arrow buddy
+                showing.setVisibility(View.VISIBLE);
+
+                Log.v("button", type + "clicked");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            showEFLA(buildings, map);
         }
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated. Zooms into the phone's current
-     * detected location.
-     * <p/>
-     * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maplap))
-                    .getMap();
-            mMap.setMyLocationEnabled(true);
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                //setUpMap();
-
-                mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-
-                    @Override
-                    public void onMyLocationChange(Location location) {
-                        // Get current location
-                        LatLng myLocation = new LatLng(location.getLatitude(),
-                                location.getLongitude());
-                        // Create an animation to zoom in on location
-                        CameraUpdate zoomCamera = CameraUpdateFactory.newLatLngZoom(myLocation, 20);
-                        // Set a pin
-                        mMap.addMarker(new MarkerOptions().position(
-                                myLocation).title("You are here!"));
-                        // And do it!
-                        mMap.animateCamera(zoomCamera);
-                    }
-                });
-            }
-        }
-    }
 }
