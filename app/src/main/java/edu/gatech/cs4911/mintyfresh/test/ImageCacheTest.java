0f16ffa1e3507027e18ca47ab48a388eeb8e2525
OzuYatamutsu <scollins36@gatech.edu>
2015-03-17 09:44:03 -0400

    Implement test cases from CacheLoader

--- app/src/main/java/edu/gatech/cs4911/mintyfresh/test/ImageCacheTest.java
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/test/ImageCacheTest.java
@@ -2,9 +2,13 @@ package edu.gatech.cs4911.mintyfresh.test;
 
 import android.content.Context;
 import android.test.AndroidTestCase;
+import android.util.Log;
+
+import java.util.List;
 
 import edu.gatech.cs4911.mintyfresh.db.DBHandler;
 import edu.gatech.cs4911.mintyfresh.db.queryresponse.FloorplanMeta;
+import edu.gatech.cs4911.mintyfresh.io.CacheLoader;
 import edu.gatech.cs4911.mintyfresh.io.ImageCache;
 
 import static edu.gatech.cs4911.mintyfresh.db.DatabaseConfig.STEAKSCORP_READ_ONLY;
@@ -27,12 +31,25 @@ public class ImageCacheTest extends AndroidTestCase {
 
     }
 
+    public void testCacheLoaderLoadImages() throws Exception {
+        assertNotNull(CacheLoader.loadImages(context));
+    }
+
+    public void testCacheLoaderLoadMeta() throws Exception {
+        assertNotNull(CacheLoader.getLocalNodes(context));
+    }
+
+    public void testCacheLoaderLoadHash() throws Exception {
+        CacheLoader.loadHashes(context);
+    }
+
     public void testInstantiation() throws Exception {
         cache = new ImageCache(handler, context);
         assertNotNull(cache);
     }
 
     public void testUpdate() throws Exception {
+        cache = new ImageCache(handler, context);
         cache.update();
 
         assertTrue(cache.size() > 0);
