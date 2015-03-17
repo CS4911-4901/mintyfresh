be34b1ded6b31babaef1fff053e3bca8f6ee2c4d
Sean Collins <scollins36@gatech.edu>
2015-03-16 23:16:03 -0400

    Get FileOutputStream from application context instead

--- app/src/main/java/edu/gatech/cs4911/mintyfresh/io/ImageDownloader.java
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/io/ImageDownloader.java
@@ -1,5 +1,7 @@
 package edu.gatech.cs4911.mintyfresh.io;
 
+import android.content.Context;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -24,27 +26,30 @@ public class ImageDownloader {
      * The size of the output file buffer, in bytes.
      */
     public static final int BUFFER_SIZE = 1024;
+
     /**
-     * Downloads an image from the server into a file at the given file path.
-     * The image to download is given by the building ID and floor in the provided
-     * FloorplanMeta object. <br><br>
+     * Downloads an image from the server into a file at the data directory of the
+     * provided application context. The image to download is given by the building
+     * ID and floor in the provided FloorplanMeta object. <br><br>
      *
      * Note that this will likely be the <b>slowest operation in the app</b>, and
      * methods that need images should consult an ImageCache first!
      *
      * @param handler The database connection to use.
      * @param meta The image to download from the server.
-     * @param path The folder to download the image into.
+     * @param context A given application context.
      * @throws SQLException if the database query failed.
      * @throws IOException if there was a problem writing to the path.
      */
-    public static void downloadToImageFile(DBHandler handler, FloorplanMeta meta, String path)
+    public static void downloadToImageFile(DBHandler handler, FloorplanMeta meta, Context context)
             throws SQLException, IOException {
         int bytesOutput;
         byte[] buffer = new byte[BUFFER_SIZE];
+        String filename =  meta.getId() + "_" + meta.getLevel() + IMAGE_EXTENSION;
         InputStream blob = DBQuery.getFloorplan(handler, meta.getId(), meta.getLevel());
-        FileOutputStream output = new FileOutputStream(
-                new File(path + meta.getId() + "_" + meta.getLevel() + IMAGE_EXTENSION));
+
+        // The Android way of writing to a File!
+        FileOutputStream output = context.openFileOutput(filename, Context.MODE_PRIVATE);
 
         // Write to output stream, BUFFER_SIZE bytes at a time
         while ((bytesOutput = blob.read(buffer)) != -1) {
@@ -54,23 +59,5 @@ public class ImageDownloader {
         // We're done!
         blob.close();
         output.close();
-    }
-
-    /**
-     * Downloads an image from the server into a file at the default
-     * image file path (CacheLoader.LOCAL_IMAGE_PATH). The image to download
-     * is given by the building ID and floor in the provided FloorplanMeta object. <br><br>
-     *
-     * Note that this will likely be the <b>slowest operation in the app</b>, and
-     * methods that need images should consult an ImageCache first!
-     *
-     * @param handler The database connection to use.
-     * @param meta The image to download from the server.
-     * @throws SQLException if the database query failed.
-     * @throws IOException if there was a problem writing to the path.
-     */
-    public static void downloadToImageFile(DBHandler handler, FloorplanMeta meta)
-            throws SQLException, IOException {
-        downloadToImageFile(handler, meta, CacheLoader.LOCAL_IMAGE_PATH);
     }
 }
