5c01926cb4bdf004c4e8464233d23d95adfbb02a
kharlan3 <kharlan3@gatech.edu>
2015-04-05 15:53:57 -0400

    spinner addition.  very preliminary.  PT#86579482

--- app/src/main/java/edu/gatech/cs4911/mintyfresh/HomeScreenActivity.java
+++ app/src/main/java/edu/gatech/cs4911/mintyfresh/HomeScreenActivity.java
@@ -1,20 +1,28 @@
 package edu.gatech.cs4911.mintyfresh;
 
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 
@@ -39,6 +47,9 @@ public class HomeScreenActivity extends ActionBarActivity {
     protected AmenityFinder amenityFinder;
     private enum elvType {NONE, VENDING, BATHROOMS, PRINTERS};
     private elvType current = elvType.NONE;
+    public static boolean[] checkSelected;
+    private boolean expanded;
+    private PopupWindow pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,18 +138,71 @@ public class HomeScreenActivity extends ActionBarActivity {
         if ((current != curType) || (current == elvType.NONE)) {
             current = curType;
             LinearLayout showing = (LinearLayout) findViewById(R.id.showingLayout);
-            Spinner showingSpinner = (Spinner) findViewById(R.id.selectedSpinner);
-            ArrayAdapter<CharSequence> adapter;
+//            Spinner showingSpinner = (Spinner) findViewById(R.id.selectedSpinner);
+
+//            ArrayAdapter<CharSequence> adapter;
             Collection<String> coll = spinnerContents.values();
             List list;
             if (coll instanceof List)
                 list = (List)coll;
             else
                 list = new ArrayList(coll);
-            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-            //bathroom or printer or vending
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            showingSpinner.setAdapter(adapter);
+            final List finalList = list;
+            checkSelected = new boolean[list.size()];
+            for (int i = 0; i < checkSelected.length; i++) {
+                checkSelected[i] = false;
+            }
+
+
+
+
+            final TextView tv = (TextView) findViewById(R.id.dropDownList_SelectBox);
+            OnClickListener ocl = new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    if(!expanded){
+                        //display all selected values
+                        String selected = "";
+                        int flag = 0;
+                        for (int i = 0; i < finalList.size(); i++) {
+                            if (checkSelected[i] == true) {
+                                selected += finalList.get(i);
+                                selected += ", ";
+                                flag = 1;
+                            }
+                        }
+                        if(flag==1)
+                            tv.setText(selected);
+                        expanded =true;
+                    }
+                    else{
+                        //display shortened representation of selected values
+                        tv.setText(DropDownListAdapter.getSelected());
+                        expanded = false;
+                    }
+                }
+            };
+
+            tv.setOnClickListener(ocl);
+
+            //onClickListener to initiate the dropDown list
+            Button createButton = (Button)findViewById(R.id.dropDownList_create);
+            createButton.setOnClickListener(new OnClickListener() {
+
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    initiatePopUp((ArrayList)finalList,tv);
+                }
+            });
+
+
+
+
+
+
+
             Log.v("showefla", "hopefully set the array adapter?");
 
 
@@ -155,6 +219,52 @@ public class HomeScreenActivity extends ActionBarActivity {
             current = elvType.NONE;
             expListView.setVisibility(View.GONE);
         }
+    }
+
+    /*
+         * Function to set up the pop-up window which acts as drop-down list
+         * */
+    private void initiatePopUp(ArrayList<String> items, TextView tv){
+        LayoutInflater inflater = (LayoutInflater)HomeScreenActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        //get the pop-up window i.e.  drop-down layout
+        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.pop_up_window, (ViewGroup)findViewById(R.id.PopUpView));
+
+        //get the view to which drop-down layout is to be anchored
+        RelativeLayout layout1 = (RelativeLayout)findViewById(R.id.relativeLayout1);
+        pw = new PopupWindow(layout, ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+
+        //Pop-up window background cannot be null if we want the pop-up to listen touch events outside its window
+        pw.setBackgroundDrawable(new BitmapDrawable());
+        pw.setTouchable(true);
+
+        //let pop-up be informed about touch events outside its window. This  should be done before setting the content of pop-up
+        pw.setOutsideTouchable(true);
+        pw.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //dismiss the pop-up i.e. drop-down when touched anywhere outside the pop-up
+        pw.setTouchInterceptor(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    pw.dismiss();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        //provide the source layout for drop-down
+        pw.setContentView(layout);
+
+        //anchor the drop-down to bottom-left corner of 'layout1'
+        pw.showAsDropDown(layout1);
+
+        //populate the drop-down list
+        final ListView list = (ListView) layout.findViewById(R.id.dropDownList_dropDownList);
+        DropDownListAdapter adapter = new DropDownListAdapter(this, items, tv);
+        list.setAdapter(adapter);
     }
 
 
